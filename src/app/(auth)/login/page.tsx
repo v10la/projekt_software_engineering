aba8c8d16f28155d860144047d41c85d096c0c25
Unadvised7311 <social_d@mailbox.org>
2026-03-12 15:06:18 +0100

    Add dynamic export to login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic';
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
